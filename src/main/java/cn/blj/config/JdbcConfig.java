8e5e71416de6cd70248ce6aa03da26ee892d81b6
bailijun <18756096010@163.com>
2019-07-15 22:46:52 +0800

    add JdbcConfig.java

---
+++ src/main/java/cn/blj/config/JdbcConfig.java
@@ -0,0 +1,36 @@
+package cn.blj.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.sql.DataSource;
+
+/**
+ * java代码代替xml配置
+ */
+@Configuration
+@PropertySource("classpath:jdbcConfig.properties")
+public class JdbcConfig {
+    @Value("${jdbc.driver}")
+    private String driverClassName;
+    @Value("${jdbc.url}")
+    private String url;
+    @Value("${jdbc.username}")
+    private String username;
+    @Value("${jdbc.password}")
+    private String password;
+
+    @Bean
+    public DataSource dataSource(){
+        DruidDataSource druidDataSource = new DruidDataSource();
+        druidDataSource.setDriverClassName(driverClassName);
+        druidDataSource.setUrl(url);
+        druidDataSource.setUsername(username);
+        druidDataSource.setPassword(password);
+        return druidDataSource;
+    }
+
+}
